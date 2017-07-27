6e691fe24d123b35efa5654fbf2c205608e80b5f
weihainanmail <weihainanmail@126.com>
2017-07-27 21:59:46 +0800

    update react demo

---
+++ ReactDemo/src/components/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Component1 extends React.Component {
+    render() {
+        return (
+            <div>Hello World! by WeiHaiNan</div>
+        )
+    }
+}
+
+export default Component1;
---
+++ ReactDemo/src/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Component from './components/Component.jsx';
+
+ReactDom.render(
+    <Component />,
+    document.getElementById('content')
+);
